a340d1d228a18ef7265d9d034df6ee3463a40854
Sasha MOREL <s.morel@almtesh.net>
2016-05-22 22:57:39 +0200

    Gerenal: - Changed structure to allow other languages - Added Python2's file to ignore in .gitignore - Changed README with a language-free text
    Python2:
    - Added initial file with top
    
    Python3:
    - Added Sensor class for better reading of values
    - Changed return of HomeSFR.get_sensor () method

---
+++ python2/homesfr.py
@@ -0,0 +1,30 @@
+# -*- coding: utf8 -*-
+'''
+Home by SFR wrapping class
+Plain use of your Home by SFR device from a Python 3 library
+
+Warning:
+This is a wrap aroud website, this could stop working without prior notice
+'''
+
+# TODO:
+## Write the library
+
+authors = (
+	)
+name = 'homesfr for Python 2'
+version = '0.8-20160521'
+
+# Settable modes
+MODE_OFF = 0
+MODE_CUSTOM = 1
+MODE_ON = 2
+
+# Sensors names
+PRESENCE_DETECTOR = 'PIR_DETECTOR'
+MAGNETIC_OPENNING_DETECTOR = 'MAGNETIC'
+SMOKE_DETECTOR = 'SMOKE'
+SIREN = 'SIREN'
+REMOTE_CONTROLER = 'REMOTE'
+KEYPAD_CONTROLER = 'KEYPAD'
+PRESENCE_CAMERA_DETECTOR = 'PIR_CAMERA'
